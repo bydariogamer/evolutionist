632819a08119759fe0bc41a725553558a67b0647
bydariogamer <bydariogamer@inventati.org>
2021-12-29 20:12:15 +0100

    improve the fog system

--- src/fog.py
+++ src/fog.py
@@ -1,12 +1,12 @@
-from typing import *
-import src.tilemap as tm
-from src.utils import *
-from src.data import *
 import pygame
+from typing import List
+
+import src.tilemap as tm
+from src.data import *
+from src.utils import *
 
 
 CODE = tm.CODE
-
 
 SURFACES = {
     255: pygame.surface.Surface(TILE_SIZE),
@@ -23,46 +23,39 @@ for v in SURFACES:
 class Fog(List[List[int]]):
 
     def from_tilemap(self, tilemap: tm.TileMap):
-        self.tm = tilemap
+        self.tilemap = tilemap
         self.clear()
-        for row in self.tm:
+        for row in self.tilemap:
             self.append(len(row)*[255])
 
     @property
     def offset(self):
-        return self.tm.offset
+        return self.tilemap.offset
 
     def update(self, player):
-        j, i = (round(x) for x in (pygame.math.Vector2(player.rect.center) - self.offset).elementwise() // TILE_SIZE)
-        self[i][j] = 0
-        for [i, j] in [
-            (i, j - 1),
-            (i + 1, j),
-            (i, j + 1),
-            (i - 1, j),
-            (i + 1, j + 1),
-            (i + 1, j - 1),
-            (i - 1, j - 1),
-            (i - 1, j + 1),
-            (i + 2, j),
-            (i - 2, j),
-            (i, j + 2),
-            (i, j - 2)
-        ]:
-            if i in range(len(self)) and j in range(len(self[i])):
-                self[i][j] = 0
+        x, y = (round(coord) for coord in (pygame.math.Vector2(player.rect.center) - self.offset).elementwise() // TILE_SIZE)
+        for i in range(-3, 4):
+            for j in range(-3, 4):
+                if x + i in range(len(self[i])) and y + j in range(len(self)):
+                    self[y+j][x+i] = 191
+        for i in range(-2, 3):
+            for j in range(-2, 3):
+                if x + i in range(len(self[i])) and y + j in range(len(self)):
+                    self[y+j][x+i] = 63
+        for i in (-1, 0, 1):
+            for j in (-1, 0, 1):
+                self[y+j][x+i] = 0
 
     def draw(self, surface: pygame.surface.Surface):
         x, y = self.offset
         r = surface.get_rect()
-
         surface.blits(
-            (
+            [
                 (
                     SURFACES[tile],
                     [j*TL_W + x, i*TL_H + y]
                  ) for i, row in enumerate(self) for j, tile in enumerate(row)
                 if tile and r.colliderect([j*TL_W + x, i*TL_H + y, *TILE_SIZE])
-        )
+            ]
             , False
         )
